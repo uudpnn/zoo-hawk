b2b63ac611b9aaa0ebb72a05a5f47608c5bbdb34
l337github <admin@l337.cn>
2017-04-28 12:30:17 +0800

    Update handler_config.c
    修改配置方式

--- src/scanReportProbe/handler_config.c
+++ src/scanReportProbe/handler_config.c
@@ -1,87 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libconfig.h>
-#include "handler_config.h" 
+#include "handler_config.h"
 
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE -1
+//#define EXIT_SUCCESS 0
+//#define EXIT_FAILURE -1
 #define CONFIGFILE "scanReportProbe.conf"
 
 #define GET_STRING_CONFIG_INTERFACE 0
 #define GET_STRING_CONFIG_PKG_TYPE 1
 #define GET_STRING_CONFIG_CURL_CONFIG 2
+#define GET_STRING_CONFIG_LOC_ADAPTER 3
+char *INTERFACE_TMP;
+char *PKG_TYPE_TMP;
+char *URL;		//URL of the server
+char *loc_Adapter; //local adapter name
 
-extern char *INTERFACE=NULL;
-extern char *PKG_TYPE=NULL;
-extern scp SCP;
+config_t cfg;
 
 
-int init_parameters(int varType, char *variableName){
-  config_t cfg;
-  config_setting_t *setting;
-  config_init (&cfg); //initializes the config_t structure pointed by config as a new,empty configuration
-  
- 
-  // Read and parse the file scanReportProbe.conf into the configuration object cfg. If there is an error, report it and exit.
-  //It returns an int : CONFIG_TRUE on success, or CONFIG_FALSE on failure; the config_error_text() and config_error_line() functions, can be used to obtain information about the error. 
-  if(! config_read_file(&cfg, CONFIGFILE))
-  {
-    fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),config_error_line(&cfg), config_error_text(&cfg));
+int init_get_config_parameters(){
+
+    //config_t cfg;
+    config_setting_t *setting;
+    config_init(&cfg); //initializes the config_t structure pointed by config as a new,empty configuration
+
+    // Read and parse the file scanReportProbe.conf into the configuration object cfg. If there is an error, report it and exit.
+    //It returns an int : CONFIG_TRUE on success, or CONFIG_FALSE on failure; the config_error_text() and config_error_line() functions, can be used to obtain information about the error.
+    if(! config_read_file(&cfg, CONFIGFILE))
+    {
+        fprintf(stderr,"----------------------------------------------------\n");
+        fprintf(stderr, "read config file error :%s:%d - %s\n", config_error_file(&cfg),config_error_line(&cfg), config_error_text(&cfg));
+        fprintf(stderr,"----------------------------------------------------\n");
+        config_destroy(&cfg);
+        return(EXIT_FAILURE);
+    }
+    setting= config_lookup(&cfg,"base_conf");
+    if (setting == NULL) {
+        fprintf(stderr, "No 'base_conf' setting in configuration file scanReportProbe.conf.\n");
+        //config_destroy(&cfg);
+        return (EXIT_FAILURE);
+    } else {
+        (config_lookup_string(&setting, "interface", &INTERFACE_TMP));
+        (config_lookup_string(&setting, "loc_Adapter", &loc_Adapter));
+        //printf("pkg_type : %s\n\n", INTERFACE_TMP);
+        //config_destroy(&cfg);
+        return (EXIT_SUCCESS);
+    }
+
+
+
+}
+int config_destroy_init(){
     config_destroy(&cfg);
-    return(EXIT_FAILURE);
-  }
-  
-  switch (varType) {
-        case (GET_STRING_CONFIG_INTERFACE):
-          setting = config_lookup(&cfg, INTERFACE);
-          if (setting == NULL) {fprintf(stderr, "No 'Interface' setting in configuration file scanReportProbe.conf.\n");
-                                config_destroy(&cfg);
-                                return (EXIT_FAILURE);
-                                }
-          else  {(config_lookup_string(&cfg, "interface", &INTERFACE));
-                 printf("Interface name : %s\n\n", INTERFACE); 
-                 config_destroy(&cfg);
-                 return (EXIT_SUCCESS);
-                };
-        break;
-        
-        case (GET_STRING_CONFIG_PKG_TYPE):
-          setting = config_lookup(&cfg, PKG_TYPE);
-          if (setting == NULL){fprintf(stderr, "No 'pkg_type' setting in configuration file scanReportProbe.conf.\n");
-                               config_destroy(&cfg);
-                               return (EXIT_FAILURE);
-                               }
-          else {(config_lookup_string(&cfg, "pkg_type", &PKG_TYPE)); 
-                printf("pkg_type : %s\n\n", PKG_TYPE); 
-                config_destroy(&cfg);
-                return (EXIT_SUCCESS);
-               };
-        break;
-        
-        case (GET_STRING_CONFIG_CURL_CONFIG):
-        setting = config_lookup(&cfg, &SCP);
-        if (setting == NULL){fprintf(stderr, "No 'config_parameters_structure' setting in configuration file scanReportProbe.conf.\n");
-                               config_destroy(&cfg);
-                               return (EXIT_FAILURE);
-                               }
-          else {
-  
-  
-  
-        break;
- /*
- scp = {
-      scp.URL= *config.URL,
-      scp.CODE= *config.CODE,
-      scp.VALIDATOR= *config.VALIDATOR,
-      scp.PATH_CMX= *config.PATH_CMX,
-      scp.SECRET= *config.SECRET,
-      scp.MAC_AP= *config.MAC_AP;
-       };
-  };
-  */
-  
-
-config_destroy(&cfg);//destroys the configuration config, deallocating all memory associated with the configuration, but does not attempt to deallocate the config_t structure itself
-return(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
